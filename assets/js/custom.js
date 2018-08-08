3b51d86a29899485fa6dc2ecded525a25d9887af
AlicePrincess <706654794@qq.com>
2018-08-08 17:28:54 +0800

    修改bg

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,3 +15,25 @@ window.onload=function(){
     $("#banner img").attr("src",$("#banner img").attr("src").split("img/")[0]+"img/"+arr[index]+".jpg");
     
 }
+var cols = ['#fffff','#ededed','#ececec'];
+var stars = 250;
+
+for (var i = 0; i <= stars; i++) {
+
+	var size = Math.random()*3;
+  var color = cols[parseInt(Math.random()*4)];
+
+	$('#starsBox').prepend('<span style=" width: ' + size + 'px; height: ' + size + 'px; top: ' + Math.random()*100 + '%; left: ' + Math.random()*100 + '%; background: ' + color + '; box-shadow: 0 0 '+ Math.random()*10 +'px' + color + ';"></span>') ;
+};
+
+setTimeout(function(){ 
+	$('#starsBox span').each(function(){  
+ 		$(this).css('top', Math.random()*100 + '%').css('left', Math.random()*100 + '%'); 
+  });
+}, 1);
+
+setInterval(function(){ 
+	$('#starsBox span').each(function(){  	
+ 		$(this).css('top', Math.random()*100 + '%').css('left', Math.random()*100 + '%'); 
+  });
+}, 100000); 
